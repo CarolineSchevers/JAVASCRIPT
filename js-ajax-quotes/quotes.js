9bba03f2e148bf3872d731c025749efc7f337198
CarolineSchevers <scheverscaroline@gmail.com>
2019-09-20 09:45:04 +0200

    update

--- js-ajax-quotes/quotes.js
+++ js-ajax-quotes/quotes.js
@@ -3,44 +3,32 @@ var getQuote = document.getElementById("quote");
 const baseURL = 'https://thatsthespir.it/api';
 
 
-var requestImage = new XMLHttpRequest();
-requestImage.open("GET", baseURL, true);
-
-requestImage.onreadystatechange = function refreshQuote() {
-    if (this.readyState <= 3) {
-        var output = '<div class="loader"></div>'
-        console.log('loading')
-    };
-    if (this.readyState === 4 && this.status == 200) {
-        var x = JSON.parse(this.responseText);
-        var output = "";
-
-        if (x.photo == "") {
-            output +=
+    var requestImage = new XMLHttpRequest();
+    requestImage.open("GET", baseURL , true);
+    
+    requestImage.onreadystatechange = function refreshQuote() {
+        if(this.readyState <= 3) {
+            var output = '<div class="loader"></div>'
+            console.log('loading')
+        };
+        if(this.readyState === 4 && this.status == 200){
+            var x = JSON.parse(this.responseText);
+            var output = "";
+                output += 
                 '<div class="quoteBox">' +
-                '<img class="images" src="images/user.png">' +
+                '<img src="' +x.photo+ '">' + 
                 '<p class="quote-text"><img src="images/quoteL.svg" class="quotemark">' + x.quote + '<img src="images/quoteR.svg" class="quotemark qR"></p>' +
                 '<p class="quote-author">' + "~  " + x.author + " ( " + x.total_quotes + " quotes ) </p>" +
                 '</div>'
-        } else {
-            output +=
-                '<div class="quoteBox">' +
-                '<img class="images" src="' + x.photo + '">' +
-                '<p class="quote-text"><img src="images/quoteL.svg" class="quotemark">' + x.quote + '<img src="images/quoteR.svg" class="quotemark qR"></p>' +
-                '<p class="quote-author">' + "~  " + x.author + " ( " + x.total_quotes + " quotes ) </p>" +
-                '</div>'
+            };
+            document.getElementById("quote").innerHTML = output;
         }
-    };
-    document.getElementById("quote").innerHTML = output;
-}
-
-requestImage.send();
-
-btn.addEventListener("click", () => {
-    refreshQuote()
-});
-
-function refreshQuote() {
-    requestImage.open("GET", baseURL, true);
+    
     requestImage.send();
-}
+
+    btn.addEventListener("click", () => {refreshQuote()});
+
+    function refreshQuote() {
+        requestImage.open("GET", baseURL, true);
+        requestImage.send();
+    }
